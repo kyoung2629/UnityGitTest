e3973b17f4cedecb68a1e10ef9b21a58d06ce01b
kyoung2629 <37364202+kyoung2629@users.noreply.github.com>
2022-08-02 08:14:24 +0900

    Update test1.cs

--- Assets/test1.cs
+++ Assets/test1.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class test1 : MonoBehaviour
-{
+{   
+    public transform trans; 
     // Start is called before the first frame update
     void Start()
     {
